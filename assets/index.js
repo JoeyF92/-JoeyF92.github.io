d1aff7d1602626c6286c805a3deb1bb8d4d5fe93
JoeyF92 <jf-9294@protonmail.com>
2023-10-10 16:33:29 +0100

    fixed animation

--- assets/index.js
+++ assets/index.js
@@ -246,11 +246,12 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(data);
         if (data.success == "true") {
           loader.addEventListener("animationend", function () {
+            console.log("hello");
             check.classList.add("active");
-            // setTimeout(function () {
-            //   loader.classList.remove("active");
-            //   document.getElementById("email-form").reset();
-            // }, 5000);
+            setTimeout(function () {
+              loader.classList.remove("active");
+              document.getElementById("email-form").reset();
+            }, 5000);
           });
         }
       })
